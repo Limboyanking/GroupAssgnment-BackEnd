cfe7eddac3e84eae09baf59e60b3496cac670d28
ericmkchan <113152408+ericmkchan@users.noreply.github.com>
2022-11-20 09:32:17 -0500

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,6 +11,7 @@ function getErrorMessage(err) {
   if (err.message) {
     message = err.message;
   }
+
   if (err.code) {
     switch (err.code) {
       case 11000:
@@ -20,41 +21,43 @@ function getErrorMessage(err) {
       default:
         message = 'Something went wrong';
     }
-  } 
+  }
+
   if (err.errors) {
     for (let errName in err.errors) {
-        if (err.errors[errName].message) 
+      if (err.errors[errName].message) 
         message = err.errors[errName].message;
     }
   }
 
   return message;
+
 };
 
 module.exports.signup = function(req, res, next) {
 
-    let user = new User(req.body);
-    user.provider = 'local';
-    // console.log(user);
+  let user = new User(req.body);
+  user.provider = 'local';
+  // console.log(user);
 
-    user.save((err) => {
-      if (err) {
-        let message = getErrorMessage(err);
+  user.save((err) => {
+    if (err) {
+      let message = getErrorMessage(err);
 
-        return res.status(400).json(
-          {
-            success: false, 
-            message: message
-          }
-        );
-      }
-      return res.json(
+      return res.status(400).json(
         {
-          success: true, 
-          message: 'User created successfully!'
+          success: false, 
+          message: message
         }
       );
-    });
+    }
+    return res.json(
+      {
+        success: true, 
+        message: 'User created successfully!'
+      }
+    );
+  });
 };
 
 module.exports.signin = function(req, res, next){
@@ -64,59 +67,63 @@ module.exports.signin = function(req, res, next){
     try {
       if (err || !user) {
         return res.status(400).json(
-            { 
-              success: false, 
-              message: err || info.message
-            }
-          );
+          { 
+            success: false, 
+            message: err || info.message
+          }
+        );
       }
   
       req.login(
-          user,
-          { session: false },
-          async (error) => {
-            if (error) {
-              return next(error);
-            }
+        user,
+        { session: false },
+        async (error) => {
 
-            // Generating the JWT token.
-            const payload = 
-              { 
-                id: user._id, 
-                email: user.email 
-              };
-            const token = jwt.sign(
-              { 
-                payload: payload
-              }, 
-              config.SECRETKEY, 
-              { 
-                algorithm: 'HS512', 
-                expiresIn: "20min"
-              }
-            );
-    
-            return res.json(
-              { 
-                success: true, 
-                token: token 
-              }
-            );
+          if (error) {
+            return next(error);
           }
-        );
-      } catch (error) {
 
-        console.log(error);
-        return res.status(400).json(
-          { 
-            success: false, 
-            message: getErrorMessage(error)
-          });
-      }
+          // Generating the JWT token.
+          const payload = {
+            id: user._id,
+            email: user.email
+          };
+
+          const token = jwt.sign(
+            {
+              payload: payload
+            },
+
+            config.SECRETKEY,
+
+            { 
+              algorithm: 'HS512', 
+              expiresIn: "20min"
+            }
+          );
+    
+          return res.json(
+            { 
+              success: true, 
+              token: token 
+            }
+          );
+        }
+    );
+    } catch (error) {
+
+      console.log(error);
+
+      return res.status(400).json(
+        { 
+          success: false, 
+          message: getErrorMessage(error)
+        }
+      );
     }
+  }
   )(req, res, next);
 }
-
 
 exports.myprofile = async function(req, res, next){
 
@@ -128,13 +135,15 @@ exports.myprofile = async function(req, res, next){
     res.status(200).json(me)
 
   } catch (error) {
+
     console.log(error);
-      return res.status(400).json(
-          { 
-              success: false, 
-              message: getErrorMessage(error)
-          }
-      );
+
+    return res.status(400).json(
+      { 
+        success: false, 
+        message: getErrorMessage(error)
+      }
+    );
   }
 
 }
