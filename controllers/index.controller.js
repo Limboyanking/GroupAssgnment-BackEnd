b71a5b421b1cf5ed714dd39e11734afa0c9a2aa1
Limboyanking <yanking3516@gmail.com>
2022-11-22 19:33:03 -0500

    fixing indexcontroller

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,4 +1,11 @@
-module.exports.landing = function(req, res, next) {
+let User = require('../models/user.model');
+let passport = require('passport');
+let jwt = require('jsonwebtoken');
+let config = require('../config/config');
+const { errorHandlerMiddleware } = require('../config/error-handler');
+
+module.exports.landing = function(err, req, res, next) {
+
     if (err) {
         let message = getErrorMessage(err);
 
