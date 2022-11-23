a48843e5a118e622a18daf43cff5e29249719b22
Limboyanking <yanking3516@gmail.com>
2022-11-22 19:42:18 -0500

    fix landing logic

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,26 +1,23 @@
-let User = require('../models/user.model');
-let passport = require('passport');
-let jwt = require('jsonwebtoken');
-let config = require('../config/config');
-const { errorHandlerMiddleware } = require('../config/error-handler');
 
-module.exports.landing = function(err, req, res, next) {
-
-    if (err) {
-        let message = getErrorMessage(err);
-
-        return res.status(400).json(
+module.exports.landing = function(req, res, next) {
+    try{
+        return res.json(
             {
-                success: false, 
-                message: message
+                success: true, 
+                message: 'Landing successfully!'
             }
-        );
+        ); 
     }
-    return res.json(
-        {
-            success: true, 
-            message: 'Landing successfully!'
+    catch(err){
+        if (err) {
+            let message = getErrorMessage(err);
+    
+            return res.status(400).json(
+                {
+                    success: false, 
+                    message: message
+                }
+            );
         }
-    ); 
-
+    }
 };
